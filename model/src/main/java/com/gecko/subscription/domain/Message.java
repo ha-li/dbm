06e85a258b98a97644b2cbedc8f2473358afc535
ha-li <ha-li@gmail.com>
2017-07-08 22:41:09 -0700

    add jndi, jpa resources and model obj

--- model/src/main/java/com/gecko/subscription/domain/Message.java
+++ model/src/main/java/com/gecko/subscription/domain/Message.java
@@ -1,7 +1,37 @@
 package com.gecko.subscription.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * Created by hlieu on 07/8/17.
  */
+@Entity (name="SUB_MESSAGES")
 public class Message {
+   @Id
+   @GeneratedValue
+   private Long id;
+
+   private String text;
+
+
+   public Long getId () {
+      return id;
+   }
+
+   public void setId (Long id) {
+      this.id = id;
+   }
+
+   public String getText () {
+      return text;
+   }
+
+   public void setText (String text) {
+      this.text = text;
+   }
+
+
+
 }
