c2c767b0c7bd7c3672c01b3696763b38ddc275b4
ha-li <ha-li@gmail.com>
2017-07-11 22:19:38 -0700

    add test, clean up save, update, make it reuseable

--- applications/src/main/java/com/gecko/app/subscription/SubscriptionApp.java
+++ applications/src/main/java/com/gecko/app/subscription/SubscriptionApp.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.transaction.UserTransaction;
+import java.util.List;
 
 /**
  * Created by hlieu on 07/8/17.
@@ -28,24 +29,50 @@ public class SubscriptionApp {
    }
 
 
-   public void insertMessage () throws Exception {
+   public void updateMessage (Message message) throws Exception {
       UserTransaction tx = TMS.getUserTransaction ();
       tx.begin();
 
       EntityManager em = EMF.createEntityManager ();
 
-      // em is thread safe
-      Message message = new Message ();
-      //message.setId (1L);
-      message.setText ("Hello World!");
+      em.merge (message);
+      tx.commit ();
+      em.close();
+   }
+
+   public List<Message> getMessages () throws Exception {
+      UserTransaction tx = TMS.getUserTransaction ();
+      tx.begin ();
+
+      EntityManager em = EMF.createEntityManager ();
+      List<Message> list = em.createQuery ("select m from com.gecko.subscription.domain.Message m ").getResultList ();
+
+      tx.commit ();
+      em.close();
+      return list;
+   }
+
+   public void saveMessage (Message message) throws Exception {
+      UserTransaction tx = TMS.getUserTransaction ();
+      tx.begin ();
+
+      EntityManager em = EMF.createEntityManager ();
       em.persist (message);
+
       tx.commit ();
       em.close();
    }
 
    public static void main (String[] args) throws Exception {
       SubscriptionApp app = new SubscriptionApp ();
-      app.insertMessage();
 
+      Message message = new Message();
+      message.setText ("Hello World!");
+      app.saveMessage(message);
+
+      List<Message> list = app.getMessages();
+
+      list.get(0).setText("I'm here to rule the world!");
+      app.updateMessage (list.get(0));
    }
 }
---
+++ applications/src/test/java/com/gecko/app/subscription/SubscriptionAppTest.java
@@ -0,0 +1,45 @@
+package com.gecko.app.subscription;
+
+import com.gecko.subscription.domain.Message;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Created by hlieu on 07/11/17.
+ */
+public class SubscriptionAppTest {
+
+/*
+   @Test
+   public void testInsert () throws Exception {
+      SubscriptionApp app = new SubscriptionApp ();
+      Message message = new Message();
+      message.setText ("Hello World!");
+      app.saveMessage(message);
+
+      List<Message> list = app.getMessages ();
+      Assert.assertEquals (list.get(0).getText(), "Hello World!");
+      Assert.assertEquals (list.get(0).getId ().longValue (), 1L);
+   }
+*/
+
+   @Test
+   public void testUpdate () throws Exception {
+      SubscriptionApp app = new SubscriptionApp ();
+      Message message = new Message();
+      message.setText ("Hello World!");
+      app.saveMessage(message);
+
+      List<Message> list = app.getMessages ();
+      Assert.assertEquals (list.get(0).getText(), "Hello World!");
+      Assert.assertEquals (list.get(0).getId ().longValue (), 1L);
+
+      message.setText("Bottoms up!");
+      app.updateMessage (message);
+      List<Message> list2 = app.getMessages ();
+      Assert.assertEquals (list2.get(0).getText(), "Bottoms up!");
+      Assert.assertEquals (list2.get(0).getId ().longValue (), 1L);
+   }
+}
